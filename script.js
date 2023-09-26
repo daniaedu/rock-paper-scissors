4831a4cec058724df1d97006d0c182ff06d71639
daniaedu <rocketbookdania@gmail.com>
2023-09-25 19:06:28 -0500

    Refactor game logic to remove alerts and use DOM for score/results

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 
 let playerScore = 0;
 let computerScore = 0;
+let result = document.querySelector('div#result p');
 
 // Event listeners for Rock, Paper, Scissors button
 const rockButton = document.querySelector('#rock').addEventListener('click', getPlayerChoice);
 const paperButton = document.querySelector('#paper').addEventListener('click', getPlayerChoice);
 const scissorsButton = document.querySelector('#scissors').addEventListener('click', getPlayerChoice);
 
-determineWinner(playerScore, computerScore);
+//determineWinner(playerScore, computerScore);
 
 // Returns playerChoice based on the button that was clicked and stores button ID name
 function getPlayerChoice(event) {
     let playerChoice = event.target.id;
-    return playerChoice;
+    return game(playerChoice);
 } 
 
 // Function gets computer's choice via Math.random
@@ -32,43 +33,52 @@ function getComputerChoice() {
 }
 
 // Function plays a round of Rock Paper Scissors each time it's called and updates score
-function game() {
-    let playerSelection = getPlayerChoice();
+function game(playerChoice) {
+    let playerSelection = playerChoice;
     let computerSelection= getComputerChoice();
 
     if (playerSelection === computerSelection) {
         playerScore += 1;
         computerScore += 1;
-        alert('It is a tie!');
+        document.querySelector('.player').textContent = playerScore;
+        document.querySelector('.computer').textContent = computerScore;
+        result.textContent = 'It is a tie!';
     }
     else if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
-            alert(`${playerSelection} beats ${computerSelection}! Player Wins!`);
+            result.textContent = `${playerSelection} beats ${computerSelection}! Player Wins!`;
             playerScore += 1;
+            document.querySelector('.player').textContent = playerScore;
         }
         else {
-            alert(`${computerSelection} beats ${playerSelection}! Computer Wins!`);
+            result.textContent =`${computerSelection} beats ${playerSelection}! Computer Wins!`;
             computerScore += 1;
+            document.querySelector('.computer').textContent = computerScore;
         }
     }
     else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            alert(`${playerSelection} beats ${computerSelection}! Player Wins!`);
+
+            result.textContent =`${playerSelection} beats ${computerSelection}! Player Wins!`;
             playerScore += 1;
+            document.querySelector('.player').textContent = playerScore;
         }
         else {
-            alert(`${computerSelection} beats ${playerSelection}! Computer Wins!`);
+            result.textContent =`${computerSelection} beats ${playerSelection}! Computer Wins!`;
             computerScore += 1;
+            document.querySelector('.computer').textContent = computerScore;
         }
     }
     else {
         if (computerSelection === 'paper') {
-            alert(`${playerSelection} beats ${computerSelection}! Player Wins!`);
+            result.textContent =`${playerSelection} beats ${computerSelection}! Player Wins!`;
             playerScore += 1;
+            document.querySelector('.player').textContent = playerScore;
         }
         else {
-            alert(`${computerSelection} beats ${playerSelection}! Computer Wins!`);
+            result.textContent =`${computerSelection} beats ${playerSelection}! Computer Wins!`;
             computerScore += 1;
+            document.querySelector('.computer').textContent = computerScore;
         }
     }
 }
